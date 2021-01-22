245165098bf114c98ddbdff5e4a48245bf4e126f
Eliseu Carneiro <eliseumga@gmail.com>
2021-01-22 13:32:32 -0300

    teste

--- helpers/initDB.js
+++ helpers/initDB.js
@@ -4,7 +4,6 @@ const connection = {};
 
 async function initDB() {
     if (connection.isConnected) {
-        console.log('Banco conectado com sucesso!');
         return;
     }
 
@@ -17,27 +16,3 @@ async function initDB() {
 }
 
 export default initDB;
-
-/*
-import mongoose from 'mongoose';
-
-function initDB() {
-    if (mongoose.connections[0].readyState) {
-        console.log('Já esta conectado ao mongo...')
-        return
-    }
-    mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true
-    });
-    mongoose.connection.on('connected', () => {
-        console.log('Conectado ao mongo...');
-    });
-    mongoose.connection.on('error', (err) => {
-        console.log('Erro ao conectar ao mongo...', err)
-    });
-}
-
-export default initDB;
-*/
